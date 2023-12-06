ec410a06331e17157c3b9b58f3c0943a8b69242a
jasonirvine76 <jason.irvine@ui.ac.id>
2023-12-06 16:15:24 +0700

    feat: add CI/CD step

--- search_machine/searching_module/RealQuery.py
+++ search_machine/searching_module/RealQuery.py
@@ -10,7 +10,7 @@ def ask(request):
     query = request
     BSBI_instance = BSBIIndex(data_dir='search_machine/searching_module/data/collections',
                               postings_encoding=VBEPostings,
-                              output_dir='search_machine/searching_module/data/index')
+                              output_dir='/src/search_machine/searching_module/data/index')
     current_ranker = Ranker()
     current_ranker.load()
     
